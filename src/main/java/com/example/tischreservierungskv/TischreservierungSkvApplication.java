4ed1cec0e8a7315c128c8bfcb6b243e897fa51c0
DominikR02 <125642128+richti03@users.noreply.github.com>
2025-11-08 11:37:28 +0100

    Fix Spring Boot App

--- src/main/java/com/example/tischreservierungskv/TischreservierungSkvApplication.java
+++ src/main/java/com/example/tischreservierungskv/TischreservierungSkvApplication.java
@@ -3,10 +3,34 @@ package com.example.tischreservierungskv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 @SpringBootApplication
 public class TischreservierungSkvApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TischreservierungSkvApplication.class, args);
+        browse("http://localhost:8080/");
+    }
+
+    public static void browse(String url) {
+        if(Desktop.isDesktopSupported()){
+            Desktop desktop = Desktop.getDesktop();
+            try {
+                desktop.browse(new URI(url));
+            } catch (IOException | URISyntaxException e) {
+                e.printStackTrace();
+            }
+        }else{
+            Runtime runtime = Runtime.getRuntime();
+            try {
+                runtime.exec("rundll32 url.dll,FileProtocolHandler " + url);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
